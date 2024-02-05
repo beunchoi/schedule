e5078e44c56ae10f830f23dfccbfbb2ec346e53e
beunchoi <byungeun0228@gmail.com>
2024-02-05 15:51:36 +0900

    부분 수정

--- src/main/java/com/sparta/schedule/jwt/HomeController.java
+++ src/main/java/com/sparta/schedule/jwt/HomeController.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String home(Model model, @AuthenticationPrincipal UserDetailsImpl userDetails) {
-        model.addAttribute("username", userDetails.getUsername());
+    public String home() {
+
         return "redirect:/api/schedules";
     }
 }
