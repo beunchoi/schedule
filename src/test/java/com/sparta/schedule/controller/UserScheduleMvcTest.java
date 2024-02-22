2efda8d6e36dce491bdceac455d0369714dc0b8a
beunchoi <byungeun0228@gmail.com>
2024-02-22 17:15:41 +0900

    controller 테스트 작성

---
+++ src/test/java/com/sparta/schedule/controller/UserScheduleMvcTest.java
@@ -0,0 +1,131 @@
+package com.sparta.schedule.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.schedule.config.WebSecurityConfig;
+import com.sparta.schedule.dto.ScheduleRequestDto;
+import com.sparta.schedule.entity.User;
+import com.sparta.schedule.entity.UserRoleEnum;
+import com.sparta.schedule.security.UserDetailsImpl;
+import com.sparta.schedule.service.ScheduleService;
+import com.sparta.schedule.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.security.Principal;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(
+        controllers = {UserController.class, ScheduleController.class},
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.ASSIGNABLE_TYPE,
+                        classes = WebSecurityConfig.class
+                )
+        }
+)
+
+class UserScheduleMvcTest {
+    private MockMvc mvc;
+
+    private Principal mockPrincipal;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    UserService userService;
+
+    @MockBean
+    ScheduleService scheduleService;
+
+    @BeforeEach
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity(new MockSpringSecurityFilter()))
+                .build();
+    }
+
+    private void mockUserSetup() {
+
+        String username = "beunchoi";
+        String password = "1234";
+        String email = "asd@email.com";
+        UserRoleEnum role = UserRoleEnum.USER;
+        User testUser = new User(username, password, email, role);
+        UserDetailsImpl testUserDetails = new UserDetailsImpl(testUser);
+        mockPrincipal = new UsernamePasswordAuthenticationToken(testUserDetails, "", testUserDetails.getAuthorities());
+    }
+
+    @Test
+    @DisplayName("로그인 Page")
+    void test1() throws Exception {
+        // when - then
+        mvc.perform(get("/api/user/login-page"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("회원 가입 요청 처리")
+    void test2() throws Exception {
+        // given
+        MultiValueMap<String, String> signupRequestForm = new LinkedMultiValueMap<>();
+        signupRequestForm.add("username", "beunchoi");
+        signupRequestForm.add("password", "1234");
+        signupRequestForm.add("email", "asd@email.com");
+        signupRequestForm.add("admin", "false");
+
+        // when - then
+        mvc.perform(post("/api/user/signup")
+                        .params(signupRequestForm)
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/api/user/login-page"))
+                .andDo(print());
+    }
+
+    @Test
+    @DisplayName("새로운 일정 등록")
+    void test3() throws Exception {
+        // given
+        this.mockUserSetup();
+        String title = "오늘 할 일";
+        String contents = "운동";
+        ScheduleRequestDto requestDto = new ScheduleRequestDto(title, contents);
+
+        String postInfo = objectMapper.writeValueAsString(requestDto);
+
+        // when - then
+        mvc.perform(post("/api/schedules")
+                        .content(postInfo)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON)
+                        .principal(mockPrincipal)
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+    }
+}
